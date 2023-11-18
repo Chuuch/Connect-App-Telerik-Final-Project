11c5eabe4b01ae56c7b7691ebe23720edb0e8e91
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-18 22:59:36 +0200

    Image path fixed

--- src/components/SidebarHeader/SidebarHeader.tsx
+++ src/components/SidebarHeader/SidebarHeader.tsx
@@ -3,7 +3,7 @@
 export const SidebarHeader = () => {
   return (
     <div className="bg-white/90 w-96 h-24 flex items-center">
-        <img src='../../../public/Images/dani.jpeg' alt="profile-pic" className="rounded-full h-14 w-14 ml-4" />
+        <img src='/Images/dani.jpeg' alt="profile-pic" className="rounded-full h-10 w-10 ml-4" />
         <p className="ml-2 text-blue-500">Daniel Chuchulev</p>
     </div>
   )
