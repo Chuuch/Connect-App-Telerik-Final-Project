15f33d1e0e93ae7095e026082ecd666392db64f4
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-18 22:54:16 +0200

    ChatHeader styling and positioning

--- src/components/ChatHeader/ChatHeader.tsx
+++ src/components/ChatHeader/ChatHeader.tsx
@@ -6,12 +6,12 @@ import { IoPersonAdd } from 'react-icons/io5';
 export const ChatHeader = () => {
   return (
     <div className="flex flex-row items-center justify-between bg-white/90 h-24 w-full">
-        <div className='flex flex-row items-center ml-96'>
-            <IoIosSearch size={30} className='mr-2 fill-blue-500 cursor-pointer'/>
+        <div className='flex flex-row items-center space-x-1 ml-72'>
         <input 
             type="text"
-            placeholder='Search'
-            className='rounded-xl w-96 h-8 p-2 border-2 active:border-blue-500 text-blue-500 bg-transparent' />
+            placeholder='Search in chat'
+            className='rounded-xl w-96 h-8 p-2 border-2 outline-none focus:outline-none bg-transparent' />
+            <IoIosSearch size={30} className='mr-2 fill-blue-500 cursor-pointer'/>
         </div>
         <div className='flex items-center right-0 space-x-4 mr-10'>
             <IoIosCall size={28} className='fill-blue-500 cursor-pointer'/>
