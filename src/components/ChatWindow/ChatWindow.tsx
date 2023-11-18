72d235a3c91ed12f77c0864cd683ca26e454b5f6
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-18 22:53:51 +0200

    ChatWindow stylization

---
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -0,0 +1,23 @@
+
+
+// type Props = {}
+
+import { ChatHeader } from "../ChatHeader/ChatHeader"
+import { MessageBox } from "../MessageBox/MessageBox"
+import { Messages } from "../Messages/Messages"
+
+export const ChatWindow = () => {
+  return (
+    <div className="bg-white h-screen w-full flex flex-col">
+      <div className="border-b bg-white/30 w-full">
+        <ChatHeader />
+      </div>
+      <div className="h-screen w-fit">
+        <Messages />
+      </div>
+      <div className="h-32 border-t justify-end w-full">
+        <MessageBox />
+      </div>
+    </div>
+  );
+};
