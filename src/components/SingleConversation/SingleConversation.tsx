2b4d7e07b4e30e390d970f60688fa3c82c83b480
Zheko Stanchev <zheko.stanchev@gmail.com>
2023-11-19 16:17:31 +0200

    Update - direct call/message

--- src/components/SingleConversation/SingleConversation.tsx
+++ src/components/SingleConversation/SingleConversation.tsx
@@ -1,3 +1,5 @@
+import { BsChatTextFill } from 'react-icons/bs'
+import { IoCall, IoMic, IoVideocam } from 'react-icons/io5'
 import { PiUserCircleFill } from 'react-icons/pi'
 
 
@@ -11,6 +13,10 @@ export const SingleConversation = () => {
         <li className="flex flex-row space-x-1 items-center text-md hover:bg-gray-100 cursor-pointer h-full w-96">
             <PiUserCircleFill size={60} className='fill-gray-500'/>
             <p className='text-gray-500'>Vasil Ivanov</p>
+            <IoCall className="fill-gray-500 active:fill-blue-500" size={25} />
+            <IoVideocam className="fill-gray-500 active:fill-blue-500" size={25} />
+            <IoMic className="text-gray-500 fill-gray-500 active:fill-blue-500" size={25} />
+            <BsChatTextFill className="fill-gray-500 active:fill-blue-500" size={25} />
         </li>
         <li className="flex flex-row space-x-1 items-center text-md hover:bg-gray-100 cursor-pointer h-full w-96">
             <PiUserCircleFill size={60} className='fill-gray-500 cursor-pointer'/>
