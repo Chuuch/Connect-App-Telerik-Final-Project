72a607f6b6f1f65f26fec49d8382c5f9b7047220
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 01:01:34 +0200

    ChatWindow styling adjustment

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -2,22 +2,24 @@
 
 // type Props = {}
 
-import { ChatHeader } from "../ChatHeader/ChatHeader"
+import { Conversations } from "../Conversations/Conversations"
 import { MessageBox } from "../MessageBox/MessageBox"
 import { Messages } from "../Messages/Messages"
 
 export const ChatWindow = () => {
   return (
-    <div className="bg-white h-screen w-full flex flex-col">
-      <div className="border-b bg-white/30 w-full">
-        <ChatHeader />
+    <div className="flex bg-white">
+      <div className="flex flex-col items-start justify-start h-[600px]">
+        <Conversations />
       </div>
-      <div className="h-screen w-fit">
+    <div className="h-full flex flex-col items-start justify-start">
+      <div className="h-full w-fit">
         <Messages />
       </div>
       <div className="h-32 justify-end w-full">
         <MessageBox />
       </div>
+    Co</div>
     </div>
   );
 };
