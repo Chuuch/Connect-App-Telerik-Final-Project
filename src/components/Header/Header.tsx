f7810c0befeaa53dc02aa1983640f4e50a7f90a1
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 01:01:01 +0200

    Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import { IoMdVideocam } from 'react-icons/io';
+import { IoSearch } from 'react-icons/io5';
+import { IoCall } from 'react-icons/io5';
+import { IoPersonAdd } from 'react-icons/io5';
+
+export const Header = () => {
+  return (
+    <div className="flex flex-row items-center justify-between bg-white h-24">
+      <img src="connect2.png" alt="logo" className="w-14 h-14" />
+      <div className="flex flex-row items-center w-full justify-end space-x-1">
+        <input
+          type="text"
+          placeholder="Search in chat"
+          className="rounded-full w-96 h-8 p-3 text-blue-500 border outline-none focus:outline-none bg-transparent"
+        />
+        <IoSearch size={25} className="mr-2 fill-blue-500 cursor-pointer" />
+      </div>
+      <div className="flex flex-row items-center justify-end space-x-4">
+        <IoCall
+          size={25}
+          className="fill-blue-500 hover:fill-blue-500/90 cursor-pointer"
+        />
+        <IoMdVideocam
+          size={25}
+          className="fill-blue-500 hover:fill-blue-500/90 cursor-pointer"
+        />
+        <IoPersonAdd
+          size={25}
+          className="fill-blue-500 hover:fill-blue-500/90 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
