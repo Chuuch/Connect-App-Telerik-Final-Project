3302383765bf28e084c4248783d8f91ca48dc73c
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 01:02:40 +0200

    Components order in Home View

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,16 +1,16 @@
-// type Props = {}
-
-import { ChatWindow } from '../../components/ChatWindow/ChatWindow';
+import { Outlet } from 'react-router-dom';
 import { Navbar } from '../../components/Navbar/Navbar';
-import { Sidebar } from '../../components/Sidebar/Sidebar';
+import { Header } from '../../components/Header/Header';
 
 export const Home = () => {
 	return (
-		<div className="flex flex-row h-screen w-full">
-			<Navbar />
-			<div className="flex flex-row z-1 w-full items-start justify-start">
-				<Sidebar />
-				<ChatWindow />
+		<div>
+			<div>
+				<Header />
+				<Navbar />
+			</div>
+			<div className="">
+				<Outlet />
 			</div>
 		</div>
 	);
