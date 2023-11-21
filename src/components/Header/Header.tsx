d36ed048e3c1e3a27e430adc8e411f9d56026053
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 01:05:58 +0200

    Header elements alignment

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,9 @@ import { IoPersonAdd } from 'react-icons/io5';
 
 export const Header = () => {
   return (
-    <div className="flex flex-row items-center justify-between bg-white h-24">
-      <img src="connect2.png" alt="logo" className="w-14 h-14" />
-      <div className="flex flex-row items-center w-full justify-end space-x-1">
+    <div className="flex flex-row items-center bg-white h-24">
+      <img src="connect2.png" alt="logo" className="w-14 h-14 ml-5" />
+      <div className="flex flex-row items-center w-full justify-center space-x-1 ml-60">
         <input
           type="text"
           placeholder="Search in chat"
@@ -15,7 +15,7 @@ export const Header = () => {
         />
         <IoSearch size={25} className="mr-2 fill-blue-500 cursor-pointer" />
       </div>
-      <div className="flex flex-row items-center justify-end space-x-4">
+      <div className="flex flex-row items-center space-x-4 mr-10">
         <IoCall
           size={25}
           className="fill-blue-500 hover:fill-blue-500/90 cursor-pointer"
