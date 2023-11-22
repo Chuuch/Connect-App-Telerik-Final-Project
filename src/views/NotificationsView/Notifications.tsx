b818b83dbcbef305f299b40e86131ec5c9c9f8eb
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 18:16:26 +0200

    Notifications view added

---
+++ src/views/NotificationsView/Notifications.tsx
@@ -0,0 +1,24 @@
+
+// type Props = {}?
+
+import { MessageBox } from "../../components/MessageBox/MessageBox"
+import { Messages } from "../../components/Messages/Messages"
+import { Notifications } from "../../components/Notifications/Notifications"
+
+export const NotificationsView = () => {
+  return (
+    <div className="bg-white flex flex-row items-start justify-start h-[845px]">
+      <div className="h-full shadow-xl">
+        <Notifications />
+      </div>
+    <div className="flex flex-col w-[1440px]">
+      <div className="h-[710px] border-l shadow-inner">
+        <Messages />
+      </div>
+      <div className="border-l">
+        <MessageBox />
+      </div>
+    </div>
+    </div>
+  )
+}
