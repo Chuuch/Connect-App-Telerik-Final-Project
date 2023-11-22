b77d3a0b00596feac5de09f757d165674b27b858
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 18:14:57 +0200

    TeamsView layout

---
+++ src/views/TeamsView/TeamsView.tsx
@@ -0,0 +1,21 @@
+import { MessageBox } from "../../components/MessageBox/MessageBox"
+import { Messages } from "../../components/Messages/Messages"
+import { Teams } from "../../components/Teams/Teams"
+
+export const TeamsView = () => {
+  return (
+    <div className="bg-white flex flex-row items-start justify-start h-[845px]">
+      <div className="h-full shadow-xl">
+        <Teams />
+      </div>
+    <div className="flex flex-col w-[1440px]">
+      <div className="h-[710px] border-l shadow-inner">
+        <Messages />
+      </div>
+      <div className="border-l">
+        <MessageBox />
+      </div>
+    </div>
+    </div>
+  )
+}
