419a9f2931f4e6bea73568e5aeacbe5247a5d8e1
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 18:30:55 +0200

    Added padding and space between items on y axis

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -4,7 +4,7 @@ import { PiUserCircleFill } from "react-icons/pi"
 export const Notifications = () => {
   return (
   <div>
-    <ul className="flex flex-col">
+    <ul className="flex flex-col pt-4 space-y-2">
     <li className="flex flex-row space-x-1 items-center text-md hover:bg-gray-100 cursor-pointer h-full w-96">
         <PiUserCircleFill size={60} className='fill-gray-500 cursor-pointer'/>
         <p className='text-gray-500'>Ivan Tonchev sent you a message</p>
