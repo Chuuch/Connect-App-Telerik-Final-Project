d44b44432dcc575278d2e849b1346de0fa3e8da6
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 04:51:05 +0200

    Added border, shadow and fixed height

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -9,11 +9,11 @@ import { Messages } from "../Messages/Messages"
 export const ChatWindow = () => {
   return (
     <div className="bg-white flex flex-row items-start justify-start h-[845px]">
-      <div className="h-fit">
+      <div className="h-full shadow-xl">
         <Conversations />
       </div>
     <div className="flex flex-col w-[1440px]">
-      <div className="h-[710px] border-l">
+      <div className="h-[710px] border-l shadow-inner">
         <Messages />
       </div>
       <div className="border-l">
