470dc5cd1a1ffab808a9fb39e5fb914d75d387ab
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-22 21:50:27 +0200

    Calendar Feature + styling

--- src/components/CalendarFeature/CalendarFeature.tsx
+++ src/components/CalendarFeature/CalendarFeature.tsx
@@ -11,12 +11,23 @@ export const CalendarFeature = () => {
     const [value, onChange] = useState<Value>(new Date());
 
   return (
-<div className='p-4'>
+    <div className="div">
+      <div className="flex items-center justify-center border-b p-2">
+        <h2 className="text-blue-500 text-2xl font-bold p-2 ">Calendar</h2>
+      </div>
+    
+<div className='p-4 shadow-inner'>
   <Calendar
     onChange={onChange}
     value={value}
     className='w-full max-w-full bg-white text-gray-800 rounded-lg shadow-md font-sans leading-tight'
   />
+</div>
+<div className="flex items-center justify-center pt-5">
+  <button className='bg-blue-500 hover:bg-blue-500/90 text-white p-2 rounded-md text-sm'>
+        Create note
+      </button>
+  </div>
 </div>
 
 
