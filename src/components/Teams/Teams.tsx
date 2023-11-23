e4434a86d07c01f46fbe64f84dcd0539b9daaa15
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-23 02:11:59 +0200

    Teams layout & creation functionality implemented

--- src/components/Teams/Teams.tsx
+++ src/components/Teams/Teams.tsx
@@ -1,71 +1,104 @@
-// import { useEffect, useState } from 'react';
-// import { ref, onValue } from 'firebase/database';
-// import { db } from '../../config/firebase-config';
-import { RiTeamFill } from 'react-icons/ri';
+import { useEffect, useState } from 'react';
+import { ref, onValue } from 'firebase/database';
+import { db } from '../../config/firebase-config';
+import { createTeam } from '../../services/teams.services';
+import { SingleTeam } from '../SingleTeam/SingleTeam';
 
-// interface Team {
-//   id: string;
-//   name: string;
-//   owner: string;
-//   members: object;
-//   channels: object;
-// }
+interface Team {
+	id: string;
+	teamName: string;
+	owner: string;
+	members: object;
+	channels: object;
+	timeStamp: number;
+}
 
-export const Teams = () => {
-  // const [teams, setTeams] = useState<Team[]>([]);
+export const Teams: React.FC = () => {
+	const [teams, setTeams] = useState<Team[]>([]);
+	const [showForm, setShowForm] = useState<boolean>(false);
+	const [teamName, setTeamName] = useState<string>('');
 
-  // useEffect(() => {
-  //   const teamsRef = ref(db, 'teams');
-  //   const unsubscribe = onValue(teamsRef, (snapshot) => {
-  //     const data = snapshot.val();
-  //     if (data) {
-  //       setTeams(Object.values(data));
-  //     } else {
-  //       setTeams([]);
-  //     }
-  //   });
+	const handleCreateTeam = async (e: { preventDefault: () => void }) => {
+		e.preventDefault();
 
-  //   return () => {
-  //     unsubscribe();
-  //   };
-  // }, []);
-
-  return (
-    <div className='w-96'>
-      <div className="flex flex-row justify-center items-center border-b p-2">
-      <div className="flex items-center justify-center">
-        <h2 className="text-blue-500 text-2xl font-bold p-2 ">Teams</h2>
-      </div>
+		try {
+			const teamId = await createTeam(teamName);
+			setTeamName('');
+			console.log('Team created with ID:', teamId);
+		} catch (error) {
+      console.error('Failed to create team', error);
       
-      </div>
-      <div className="shadow-inner">
-      <ul className="flex flex-col pt-4 space-y-2">
-    <li className="flex flex-row space-x-1 items-center text-md hover:bg-gray-100 cursor-pointer h-full w-full p-2">
-        <RiTeamFill size={40} className='fill-blue-500  cursor-pointer'/>
-        <p className='text-gray-500'>Team Alpha</p>
-    </li>
-    <li className="flex flex-row space-x-1 items-center text-md hover:bg-gray-100 cursor-pointer h-full w-full p-2">
-        <RiTeamFill size={40} className='fill-blue-500 cursor-pointer'/>
-        <p className='text-gray-500'>Team Beta</p>
-    </li>
-    <li className="flex flex-row space-x-1 items-center text-md hover:bg-gray-100 cursor-pointer h-full w-full p-2">
-        <RiTeamFill size={40} className='fill-blue-500 cursor-pointer'/>
-        <p className='text-gray-500'>Team Gamma</p>
-    </li>
-  </ul>
-  <div className="flex items-center justify-center pt-5">
-  <button className='bg-blue-500 hover:bg-blue-500/90 text-white p-2 rounded-md text-sm'>
-        Create Team
+    }
+	};
+
+	useEffect(() => {
+		const teamsRef = ref(db, 'teams');
+		const unsubscribe = onValue(teamsRef, (snapshot) => {
+			const data = snapshot.val();
+			if (data) {
+				setTeams(Object.values(data));
+			} else {
+				setTeams([]);
+			}
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+
+	return (
+		<div className="w-96">
+			<div className="flex flex-row justify-center items-center border-b p-2">
+				<div className="flex items-center justify-center">
+					<h2 className="text-blue-500 text-2xl font-bold p-2 ">Teams</h2>
+				</div>
+			</div>
+			<div className="shadow-inner">
+				<ul className="flex flex-col pt-4 space-y-2">
+        {teams &&
+				teams.map((team) => (
+					<div className="team" key={team.id}>
+				<SingleTeam team={team} />
+					</div>
+				))}
+					
+				</ul>
+				<div className="flex items-center justify-center pt-5">
+					<button
+						onClick={() => setShowForm(true)}
+						className="bg-blue-500 hover:bg-blue-500/90 text-white p-2 rounded-md text-sm"
+					>
+						Create Team
+					</button>
+				</div>
+        {showForm && (
+  <div className="mt-4">
+    <form className="flex flex-col space-y-4 justify-center items-center border border-blue-500 rounded-md p-4">
+      <label htmlFor="teamName" className="text-lg font-semibold">
+        Team Name:
+      </label>
+      <input
+        type="text"
+        id="teamName"
+        placeholder="Please select a team name"
+        value={teamName}
+        onChange={(e) => setTeamName(e.target.value)}
+        onKeyDown={(e) => {if (e.key === 'Enter'){handleCreateTeam(e)}}}
+        required
+        className="px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+      />
+      <button
+        type="button"
+        onClick={handleCreateTeam}
+        className="bg-blue-500 hover:bg-blue-600 text-white w-28 px-4 py-2 rounded-md text-sm"
+      >
+        Submit
       </button>
+    </form>
   </div>
-  </div>
-      {/* <h2>Teams</h2> */}
-      {/* {teams.map((team) => (
-        <div className="team" key={team.id}>
-          <h3>{team.name}</h3>
-          <p>Owner: {team.owner}</p>
-        </div>
-      ))} */}
-    </div>
-  );
+)}
+			</div>
+		</div>
+	);
 };
