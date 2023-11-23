4ca5b7d4abe3eeb4c8a881350c2b3698aaaddaa9
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-23 23:53:23 +0200

    Drawer icon color changed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -57,7 +57,7 @@ export const Navbar = () => {
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
-								className="inline-block w-6 h-6 stroke-current"
+								className="inline-block w-6 h-6 stroke-current dark:stroke-slate-400"
 							>
 								<path
 									strokeLinecap="round"
