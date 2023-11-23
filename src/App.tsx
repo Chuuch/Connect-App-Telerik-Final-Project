7e01e33a0e228f53d31c99ae5e061bac4c7d33a3
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-23 03:25:18 +0200

    Fixed routing paths for component views

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Alert } from './components/Alert/Alert';
-import { Calls } from './components/Calls/Calls';
 import { ChatWindow } from './components/ChatWindow/ChatWindow';
 import { Loader } from './components/Loader/Loader';
-import { Notifications } from './components/Notifications/Notifications';
-import { Teams } from './components/Teams/Teams';
 import { auth } from './config/firebase-config';
 import './index.css';
 import { Home } from './views/Home/Home';
 import { Login } from './views/Login/Login';
 import { Register } from './views/Register/Register';
+import { CalendarView } from './views/CalendarView/CalendarView';
+import { CallsView } from './views/CallsView/CallsView';
+import { TeamsView } from './views/TeamsView/TeamsView';
+import { NotificationsView } from './views/NotificationsView/Notifications';
+
 
 function App() {
 	const [user, loading, error] = useAuthState(auth)
@@ -31,8 +33,8 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				{/* <Route path="/" element={<Home />}> */}
-				{/* <Route path="notifications" element={<Authenticated><NotificationsView /></Authenticated>} />
+				{/* <Route path="/" element={<Home />} />
+				<Route path="notifications" element={<Authenticated><NotificationsView /></Authenticated>} />
 		<Route path="teams" element={<Authenticated><TeamsView /></Authenticated>} />
 		<Route path="messages" element={<Authenticated><ChatWindow /></Authenticated>} />
 		<Route path="calls" element={<Authenticated><CallsView /></Authenticated>} />
@@ -42,11 +44,11 @@ function App() {
 				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/" element={user ? <Home /> : <Login />}>
-					<Route path="notifications" element={<Notifications />} />
-					<Route path="teams" element={<Teams />} />
+					<Route path="notifications" element={<NotificationsView />} />
+					<Route path="teams" element={<TeamsView />} />
 					<Route path="messages" element={<ChatWindow />} />
-					<Route path="calls" element={<Calls />} />
-					<Route path="calendar" element={<Teams />} />
+					<Route path="calls" element={<CallsView />} />
+					<Route path="calendar" element={<CalendarView />} />
 				</Route>
 			</Routes>
 		</BrowserRouter >
