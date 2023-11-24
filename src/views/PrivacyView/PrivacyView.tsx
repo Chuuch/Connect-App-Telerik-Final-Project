7812339ea4a4043d741ca3a12053b67a4293aecd
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-24 11:51:39 +0200

    Privacy settings layout view

---
+++ src/views/PrivacyView/PrivacyView.tsx
@@ -0,0 +1,10 @@
+import { Privacy } from '../../components/Privacy/Privacy';
+
+export const PrivacyView = () => {
+	return (
+		<div className="flex items-center justify-center w-full bg-white dark:bg-black">
+			<Privacy />
+		</div>
+	);
+};
+ 
