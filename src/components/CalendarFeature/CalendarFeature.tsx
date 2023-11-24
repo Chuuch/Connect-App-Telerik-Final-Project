2df514239d4f133bbb964f1cbbb585f969b0afe1
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-24 13:26:40 +0200

    Calendar feature animation

--- src/components/CalendarFeature/CalendarFeature.tsx
+++ src/components/CalendarFeature/CalendarFeature.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
+import { motion } from 'framer-motion';
 import 'react-calendar/dist/Calendar.css';
 
 type ValuePiece = Date | null;
@@ -17,19 +18,29 @@ export const CalendarFeature = () => {
 				</h2>
 			</div>
 
-			<div className="p-4 shadow-inner">
+			<motion.div
+			initial={{ y: -100, opacity: 0 }}
+			transition={{ duration: 1.2 }}
+			whileInView={{ opacity: 1, y: 0 }}
+			viewport={{ once: true }}
+			className="p-4 shadow-inner">
 				<Calendar
 					onChange={onChange}
 					value={value}
 					className="
           w-full max-w-full bg-white text-gray-800 rounded-lg shadow-md font-sans leading-tight"
 				/>
-			</div>
-			<div className="flex items-center justify-center pt-5">
+			</motion.div>
+			<motion.div
+			initial={{ y: -100, opacity: 0 }}
+			transition={{ duration: 1.2 }}
+			whileInView={{ opacity: 1, y: 0 }}
+			viewport={{ once: true }}
+			className="flex items-center justify-center pt-5">
 				<button className="bg-blue-500 hover:bg-blue-500/90 dark:bg-purple-600/90 dark:hover:bg-purple-600 text-white p-2 rounded-md text-sm">
 					Create note
 				</button>
-			</div>
+			</motion.div>
 		</div>
 	);
 };
