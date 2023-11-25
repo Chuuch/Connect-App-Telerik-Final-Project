4d749cebc6c9b61b8a9c0dd444dbc75bfda1dc69
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-25 19:54:00 +0200

    Notifications added to NotificationsView

--- src/views/NotificationsView/Notifications.tsx
+++ src/views/NotificationsView/Notifications.tsx
@@ -8,46 +8,66 @@ import { Notifications } from "../../components/Notifications/Notifications"
 import { useState, useEffect } from "react"
 import { db } from "../../config/firebase-config"
 interface Message {
-  content:string,
-  id:string,
-  author:string,
-  userID:string,
-  timestamp: number,
-  
+  content: string;
+  id: string;
+  author: string;
+  userID: string;
+  timestamp: number;
+}
+
+type NotificationType = 'message' | 'other';
+
+interface AppNotification {
+  type: NotificationType;
+  id: string;
+  author: string;
+  userID: string;
+  onNotificationsCleared: () => void;
 }
 
 export const NotificationsView = () => {
-  const [msg, setMsg]=useState<Message[]>([]);
+  const [msg, setMsg] = useState<Message[]>([]);
+  const [notification, setNotification] = useState<AppNotification[]>([]);
 
-  useEffect(()=>{
-    const msgRef=ref(db, 'messages');
-    const unsubscribe = onValue(msgRef, (snapshot) => {
+  useEffect(() => {
+    const notificationsRef = ref(db, "notifications");
+    const unsubscribe = onValue(notificationsRef, (snapshot) => {
       const data = snapshot.val();
-      if (data) {
-        setMsg(Object.values(data));
-      } else {
-        setMsg([]);
-      }
+      setNotification(data ? Object.values(data) : []);
     });
 
     return () => {
       unsubscribe();
     };
-  },[])
+  }, [notification, setNotification]);
+
+  useEffect(() => {
+    const msgRef = ref(db, 'messages');
+    const unsubscribe = onValue(msgRef, (snapshot) => {
+      const data = snapshot.val();
+      setMsg(data ? Object.values(data) : []);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <div className="bg-white dark:bg-black flex flex-grow items-start justify-start">
       <div className="h-full shadow-xl">
-        <Notifications />
+        <Notifications notification={notification} userID={""} onNotificationsCleared={function (): void {
+          throw new Error("Function not implemented.")
+        } } />
       </div>
-    <div className="flex flex-col w-full">
-      <div className="h-[710px] border-l dark:border-gray-600 shadow-inner overflow-auto">
-        <Messages msg={msg}/>
-      </div>
-      <div>
-        <MessageBox />
+      <div className="flex flex-col w-full">
+        <div className="h-[710px] border-l dark:border-gray-600 shadow-inner overflow-auto">
+          <Messages msg={msg} />
+        </div>
+        <div>
+          <MessageBox />
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
