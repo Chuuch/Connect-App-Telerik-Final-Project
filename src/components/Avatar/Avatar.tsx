1d849de112bcfee0f22510ff416b9ad66059da17
Georgi Stoychev <stoychev86@yahoo.com>
2023-11-26 17:12:42 +0000

    Style: Avatar letters center class change

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -24,23 +24,24 @@ const Avatar = ({ userID }: { userID?: string }) => {
     }, [userData?.status, userID])
 
     return (
-        <div className="chat-image avatar">
-            {userData?.avatar?.length ? (<div className="w-10 rounded-full">
-                <img className="w-10 h-10 rounded-full" src={userData?.avatar} alt="avatar" />
-                <Badge status={userData?.status} />
-            </div>) : (
-                <>
-                    {/* <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                        <span class="font-medium text-gray-600 dark:text-gray-300">JL</span>
-                    </div> */}
-                    <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden  fill-blue-500 hover:fill-blue-500/90 dark:fill-purple-600/90 hover:dark:fill-purple-600 cursor-pointer">
-                        <span className="font-medium text-gray-600 dark:text-gray-300" >{userData?.firstName?.charAt(0)?.toUpperCase() + userData?.lastName?.charAt(0)?.toUpperCase()}
-                            <Badge status={userData?.status} />
-                        </span>
+        <>
+            {userData?.avatar?.length ? (
+                <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                        <img className="w-10 h-10 rounded-full" src={userData?.avatar} alt="avatar" />
+                        <Badge status={userData?.status} />
                     </div>
-                </>
-            )}
-        </div>
+                </div >
+            ) : (
+                <div className="chat-image ">
+                    < div className="relative inline-flex items-center justify-center w-10 h-10  bg-gray-300 rounded-full dark:bg-gray-600 ">
+                        <span className="font-medium text-gray-500 dark:text-gray-300 ">{userData?.firstName?.charAt(0).toUpperCase() + userData?.lastName?.charAt(0).toUpperCase()}</span>
+                        <Badge status={userData?.status} />
+                    </div >
+                </div>
+            )
+            }
+        </>
     )
 }
 
