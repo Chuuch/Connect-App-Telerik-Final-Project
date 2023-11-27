12195a3217ffb4cc511864280eea5d49cdc9c153
Chuuch <daniel.chuchulev96@gmail.com>
2023-11-27 15:39:07 +0200

    Removed unnecessary headings from video windows

--- src/components/WebRTC/LocalVideo.tsx
+++ src/components/WebRTC/LocalVideo.tsx
@@ -8,7 +8,6 @@ interface LocalVideoProps {
 const LocalVideo: React.FC<LocalVideoProps> = ({ localVideoRef }) => {
   return (
     <div className='flex flex-col items-center justify-center mt-52 text-white dark:text-gray-300'>
-      <h2>Your Video</h2>
       <video ref={localVideoRef} autoPlay playsInline muted />
     </div>
   );
--- src/components/WebRTC/RemoteVideo.tsx
+++ src/components/WebRTC/RemoteVideo.tsx
@@ -8,7 +8,7 @@ interface RemoteVideoProps {
 const RemoteVideo: React.FC<RemoteVideoProps> = ({ remoteVideoRef }) => {
   return (
     <div className='flex items-center justify-center text-white dark:text-gray-300 mt-52'>
-      <h2>Participant's Video</h2>
+      
       <video ref={remoteVideoRef} autoPlay playsInline />
     </div>
   );
