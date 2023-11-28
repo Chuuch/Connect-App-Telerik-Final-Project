fa272806e1d01645e19549ab6acb1cf524235a60
Georgi Stoychev <stoychev86@yahoo.com>
2023-11-28 12:29:57 +0000

    Feat: Button remove team added and styles fix

--- src/components/Teams/Teams1.tsx
+++ src/components/Teams/Teams1.tsx
@@ -7,6 +7,7 @@ import { NavLink } from 'react-router-dom';
 import { getAllTeamsByUId } from '../../services/teams.services';
 import { getAllUserFriendsList } from '../../services/users.services';
 import CreateTeamForm, { UserList } from './CreateTeamForm';
+import { MdGroupAdd, MdGroupRemove } from 'react-icons/md';
 
 const Teams1 = () => {
     const form = useForm()
@@ -38,23 +39,40 @@ const Teams1 = () => {
                     transition={{ duration: 1.2 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     viewport={{ once: true }}
-                    className="flex flex-col pt-4 space-y- justify-end w-24">
-
-                    <button
-                        onClick={() => setShowForm(true)}
-                        className=" bg-blue-600 hover:bg-blue-500 dark:bg-purple-600 hover:dark:bg-purple-500 text-white p-2 rounded-md text-sm"
-                    >
-                        Create Team
-                    </button>
+                    className="flex flex-col pt-4 space-y-  w-24">
+                    <div className=" flex flex-row  w-80 justify-center">
+                        <div className="tooltip" data-tip="Add Team">
+                            <button
+                                onClick={() => setShowForm(true)}
+                                className="flex flex-row  bg-blue-600 hover:bg-blue-500 dark:bg-purple-600 hover:dark:bg-purple-500 text-white p-2 rounded-md text-sm"
+                            >
+                                <MdGroupAdd size={20} />
+                                {/* Add Team */}
+                            </button>
+                        </div>
+                    </div>
                     {teams?.length ? teams.map((team) => (
                         <>
                             {
                                 team?.channels && Object.values(team?.channels).length ? Object.values(team.channels).map(({ id, channelName }) => (
                                     <li key={id + channelName} className="menu w-96 flex-row rounded-box">
                                         <details open>
-                                            <summary>
-                                                <RiTeamFill size={20} className="fill-blue-500 dark:fill-purple-600 cursor-pointer" />
-                                                <p className="text-gray-500 dark:text-gray-300">{team.teamName}</p></summary>
+                                            <summary className="flex flex-row ">
+                                                <div className=" flex flex-row space-x-2 w-80 justify-between">
+                                                    <div className=" flex flex-row">
+                                                        <RiTeamFill size={20} className="fill-blue-500 dark:fill-purple-600 cursor-pointer" />
+                                                        <p className="text-gray-500 dark:text-gray-300">{team.teamName} </p>
+                                                    </div>
+                                                    <div className="tooltip" data-tip="Remove Team">
+                                                        <button
+                                                            onClick={() => alert('Not implemented!!!')}
+                                                            className="bg-blue-600 hover:bg-blue-500 dark:bg-purple-600 hover:dark:bg-purple-500 text-white p-2 rounded-md text-sm"
+                                                        >
+                                                            <MdGroupRemove />
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </summary>
                                             <ul>
                                                 <li>
                                                     <NavLink to={`/teams/${team.id}/${id}`}>
@@ -64,7 +82,7 @@ const Teams1 = () => {
                                                 </li>
                                             </ul>
                                         </details>
-                                    </li>
+                                    </li >
                                 )) : null
                             }
                         </>
