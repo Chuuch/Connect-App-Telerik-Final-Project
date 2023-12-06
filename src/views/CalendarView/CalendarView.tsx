2d91b7a6aab75ca899d021d974d60f9dab574492
Chuuch <daniel.chuchulev96@gmail.com>
2023-12-06 11:37:39 +0200

    Adjusted messages container height & width

--- src/views/CalendarView/CalendarView.tsx
+++ src/views/CalendarView/CalendarView.tsx
@@ -5,10 +5,10 @@ import { Messages } from "../../components/Messages/Messages"
 export const CalendarView = () => {
   return (
     <div className="bg-white dark:bg-black flex flex-grow items-start justify-start">
-      <div className="h-full shadow-xl bg-gray-100 dark:bg-gray-900">
+      <div className="h-[830px] shadow-xl bg-gray-100 dark:bg-gray-900">
         <CalendarFeature />
       </div>
-    <div className="flex-grow flex-col w-[1440px]">
+    <div className="flex-grow flex-col w-[1410px]">
       <div className="border-l dark:border-gray-600 shadow-inner overflow-auto">
         <Messages msg={[]} />
       </div>
