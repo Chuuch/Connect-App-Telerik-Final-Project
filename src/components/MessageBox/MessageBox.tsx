bf00d8598a1fa2e4680d761152593be5b26ba73b
Zheko Stanchev <zheko.stanchev@gmail.com>
2023-12-07 01:07:56 +0200

    Feat: Upload file

--- src/components/MessageBox/MessageBox.tsx
+++ src/components/MessageBox/MessageBox.tsx
@@ -6,105 +6,149 @@ import { HiOutlineGif } from 'react-icons/hi2';
 import GiphySearch from '../Giphy/Giphy';
 import { API_KEY, API_SEARCH } from '../../constants/constants';
 import { useLocation, useParams } from 'react-router-dom';
+import { auth, storage } from '../../config/firebase-config';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 interface MessageBoxProps {
-	chatId: string; 
-  }
+  chatId: string;
+}
 
-  export const MessageBox: React.FC<MessageBoxProps> = ({ chatId }) => {
-	const { pathname } = useLocation();
-	const { channelId } = useParams();
-	const [msg, setMsg] = useState('');
-	const [emoji, setEmoji] = useState('');
-	const [showGif, setShowGif] = useState(false);
-	const [selectedGif, setSelectedGif] = useState('');
+export const MessageBox: React.FC<MessageBoxProps> = ({ chatId }) => {
+  const { pathname } = useLocation();
+  const { channelId } = useParams();
+  const [msg, setMsg] = useState('');
+  const [emoji, setEmoji] = useState('');
+  const [showGif, setShowGif] = useState(false);
+  const [selectedGif, setSelectedGif] = useState('');
+  const [file, setFile] = useState(null);
 
-	const handleSubmit = async (e: { preventDefault: () => void }) => {
-		e.preventDefault();
-		try {
-			const completeMessage = `${msg} ${emoji}`.trim();
-			if (completeMessage !== '' || selectedGif) {
-				const completeMessageWithGif = `${completeMessage} ${selectedGif}`;
-				if (pathname.includes('teams') && channelId) {
-					const messageId = await createChannelMsg(completeMessageWithGif, channelId);
-					console.log('Message sent with ID:', messageId);
-				} else {
-					const messageId = await createMsg(completeMessageWithGif, chatId);
-					console.log('Message sent with ID:', messageId);
-				}
-			}
-			setMsg('');
-			setEmoji('');
-			setSelectedGif('');
-		} catch (error) {
-			console.error('Message sent with error:', error);
-		}
-	};
+  const handleFileChange = (e) => {
+    if (e.target.files[0]) {
+      setFile(e.target.files[0]);
+    }
+  };
 
-	const handleGifClick = (gifUrl: string) => {
-		setSelectedGif(gifUrl);
-		setShowGif(false);
-	};
+  const handleUpload = async () => {
+    if (file) {
+      const timestamp = Date.now();
+      const fileName = timestamp + auth?.currentUser?.uid;
+      try {
+        const storageRef = ref(storage, `files/${fileName}`);
+        await uploadBytes(storageRef, file);
+        console.log('Uploaded file!');
+        const downloadURL = await getDownloadURL(storageRef);
+        //console.log('Download URL:', downloadURL);
+        setMsg(downloadURL);
+      } catch (err) {
+        console.log('Failed to upload: ' + err);
+      }
+    } else {
+      console.error('No file selected');
+    }
+  };
 
-	const closeGif = () => {
-		setShowGif(false);
-	};
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      const completeMessage = `${msg} ${emoji}`.trim();
+      if (completeMessage !== '' || selectedGif) {
+        const completeMessageWithGif = `${completeMessage} ${selectedGif}`;
+        if (pathname.includes('teams') && channelId) {
+          const messageId = await createChannelMsg(
+            completeMessageWithGif,
+            channelId
+          );
+          console.log('Message sent with ID:', messageId);
+        } else {
+          const messageId = await createMsg(completeMessageWithGif, chatId);
+          console.log('Message sent with ID:', messageId);
+        }
+      }
+      setMsg('');
+      setEmoji('');
+      setSelectedGif('');
+    } catch (error) {
+      console.error('Message sent with error:', error);
+    }
+  };
 
-	return (
-		<div className="relative flex flex-grow h-24 border-t border-l bg-gray-100 text-gray-700 dark:bg-gray-900 dark:border-gray-600 items-center space-x-2 w-full">
-			<div className="flex flex-grow items-center w-full ml-5 mr-2 space-x-2">
-				<input spellCheck="true"
-					type="text"
-					placeholder="Type something..."
-					value={msg}
-					onChange={(e) => setMsg(e.target.value)}
-					onKeyDown={(e) => {
-						if (e.key === 'Enter') {
-							handleSubmit(e);
-						}
-					}}
-					required
-					className="text-base p-5 h-10 rounded-full w-full mr-5 bg-transparent border bg-white focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:focus:border-purple-600 dark:text-gray-300 flex-grow outline-none focus:outline-none"
-				/>
-				<div className="relative flex flex-row">
-					<Emoji onEmojiSelect={setEmoji} />
-				</div>
-				<HiOutlineGif
-					onClick={() => setShowGif(true)}
-					size={30}
-					className="cursor-pointer stroke-blue-500 hover:stroke-blue-500/90 dark:stroke-purple-600/90 hover:dark:stroke-purple-600"
-				/>
-				<IoMdAttach
-					size={30}
-					className="cursor-pointer fill-blue-500 hover:fill-blue-500/90 dark:fill-purple-600/90 hover:dark:fill-purple-600"
-				/>
-				<IoMdPhotos
-					size={30}
-					className="cursor-pointer fill-blue-500 hover:fill-blue-500/90 dark:fill-purple-600/90 hover:dark:fill-purple-600"
-				/>
-			</div>
-			<div className="flex flex-row pr-5">
-				<button
-					className="p-1 w-20 h-10 text-sm rounded-md bg-blue-600 hover:bg-blue-500 dark:bg-purple-600/90 dark:hover:bg-purple-600 text-white"
-					onClick={handleSubmit}
-				>
-					Send
-				</button>
-			</div>
-			{showGif && (
-				<div className="flex flex-col fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 items-center justify-center">
-					<div className="bg-gray-100 dark:bg-gray-800 p-5 rounded-md flex flex-col items-center">
-						<GiphySearch
-							API_SEARCH={API_SEARCH}
-							API_KEY={API_KEY}
-							onGifClick={handleGifClick}
-						/>
-						<button onClick={closeGif} className="dark:text-gray-300 p-2 w-20 mt-4 bg-blue-500 hover:bg-blue-600 dark:bg-purple-600 dark:hover:bg-purple-500 rounded-md">
-							Close
-						</button>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  const handleGifClick = (gifUrl: string) => {
+    setSelectedGif(gifUrl);
+    setShowGif(false);
+  };
+
+  const closeGif = () => {
+    setShowGif(false);
+  };
+
+  return (
+    <div className="relative flex flex-grow h-24 border-t border-l bg-gray-100 text-gray-700 dark:bg-gray-900 dark:border-gray-600 items-center space-x-2 w-full">
+      <div className="flex flex-grow items-center w-full ml-5 mr-2 space-x-2">
+        <input
+          spellCheck="true"
+          type="text"
+          placeholder="Type something..."
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSubmit(e);
+            }
+          }}
+          required
+          className="text-base p-5 h-10 rounded-full w-full mr-5 bg-transparent border bg-white focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:focus:border-purple-600 dark:text-gray-300 flex-grow outline-none focus:outline-none"
+        />
+        <div className="relative flex flex-row">
+          <Emoji onEmojiSelect={setEmoji} />
+        </div>
+        <HiOutlineGif
+          onClick={() => setShowGif(true)}
+          size={30}
+          className="cursor-pointer stroke-blue-500 hover:stroke-blue-500/90 dark:stroke-purple-600/90 hover:dark:stroke-purple-600"
+        />
+        <label htmlFor="file-input" className="cursor-pointer">
+          <IoMdAttach
+            size={30}
+            className="fill-blue-500 hover:fill-blue-500/90 dark:fill-purple-600/90 hover:dark:fill-purple-600"
+          />
+        </label>
+        <input
+          type="file"
+          id="file-input"
+          onChange={handleFileChange}
+          style={{ display: 'none' }}
+        />
+        <button onClick={handleUpload}>Upload</button>
+        <IoMdPhotos
+          size={30}
+          className="cursor-pointer fill-blue-500 hover:fill-blue-500/90 dark:fill-purple-600/90 hover:dark:fill-purple-600"
+        />
+      </div>
+      <div className="flex flex-row pr-5">
+        <button
+          className="p-1 w-20 h-10 text-sm rounded-md bg-blue-600 hover:bg-blue-500 dark:bg-purple-600/90 dark:hover:bg-purple-600 text-white"
+          onClick={handleSubmit}
+        >
+          Send
+        </button>
+      </div>
+      {showGif && (
+        <div className="flex flex-col fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 items-center justify-center">
+          <div className="bg-gray-100 dark:bg-gray-800 p-5 rounded-md flex flex-col items-center">
+            <GiphySearch
+              API_SEARCH={API_SEARCH}
+              API_KEY={API_KEY}
+              onGifClick={handleGifClick}
+            />
+            <button
+              onClick={closeGif}
+              className="dark:text-gray-300 p-2 w-20 mt-4 bg-blue-500 hover:bg-blue-600 dark:bg-purple-600 dark:hover:bg-purple-500 rounded-md"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,5 +1,5 @@
-import { Key, useEffect, useRef } from "react";
-import { auth } from "../../config/firebase-config";
+import { Key, useEffect, useRef } from 'react';
+import { auth } from '../../config/firebase-config';
 import Avatar from '../Avatar/Avatar';
 //import {ref, onValue} from "firebase/database";
 
@@ -40,69 +40,112 @@ export const Messages: React.FC<MessagesProps> = ({ msg }) => {
   }, [msg]);
   return (
     <div className="pt-5 h-[735px]">
-      {msg && msg.map((m: { id: Key | null | undefined; timestamp: number; author: string; content: string; userID: string; hasGif: boolean }) => (
-        <div>
-          {m.userID === auth?.currentUser?.uid ?
-            (<div className="chat chat-end mr-5 text-sm" key={m.id}>
-
-              {/* <div className="chat-image avatar"> */}
-              {/* <div className="w-10 rounded-full">
+      {msg &&
+        msg.map(
+          (m: {
+            id: Key | null | undefined;
+            timestamp: number;
+            author: string;
+            content: string;
+            userID: string;
+            hasGif: boolean;
+          }) => (
+            <div>
+              {m.userID === auth?.currentUser?.uid ? (
+                <div className="chat chat-end mr-5 text-sm" key={m.id}>
+                  {/* <div className="chat-image avatar"> */}
+                  {/* <div className="w-10 rounded-full">
                   {currentUserDB?.avatar?.length ? (<img src={currentUserDB?.avatar} alt="avatar" className="w-10 h-10 rounded-full" />) :
                     <PiUserCircleFill size={35} className='fill-gray-500 cursor-pointer' />}
                 </div> */}
-              <Avatar userID={m.userID} />
-              {/* </div> */}
-              <div className="chat-header dark:text-gray-200">
-                {m.author}{' '}
-                <time className="text-xs opacity-50">{new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  second: "2-digit",
-                }).format(m.timestamp)}</time>
-              </div>
-              <div>
-                {m.hasGif ? (<img src={m.content}></img>)
-                  : (<div className="chat-bubble bg-blue-500 dark:bg-purple-600 text-white">{m.content}</div>)
-                }</div>
-              <div className="chat-footer opacity-50 text-xs pt-2 dark:text-gray-100">
-                Delivered
-              </div>
-            </div>
-            ) : (
-              <div className="chat chat-start ml-5 text-sm" key={m.id}>
-                <Avatar userID={m.userID} />
-                {/* <div className="chat-image avatar">
+                  <Avatar userID={m.userID} />
+                  {/* </div> */}
+                  <div className="chat-header dark:text-gray-200">
+                    {m.author}{' '}
+                    <time className="text-xs opacity-50">
+                      {new Intl.DateTimeFormat('en-US', {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        second: '2-digit',
+                      }).format(m.timestamp)}
+                    </time>
+                  </div>
+                  <div>
+                    {m.hasGif ? (
+                      <img src={m.content}></img>
+                    ) : m.content.slice(0, 4) === 'http' ? (
+                      <div className="chat-bubble bg-blue-500 dark:bg-purple-600 text-white">
+                        <a
+                          href={m.content}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {m.content}
+                        </a>
+                      </div>
+                    ) : (
+                      <div className="chat-bubble bg-blue-500 dark:bg-purple-600 text-white">
+                        {m.content}
+                      </div>
+                    )}
+                  </div>
+                  <div className="chat-footer opacity-50 text-xs pt-2 dark:text-gray-100">
+                    Delivered
+                  </div>
+                </div>
+              ) : (
+                <div className="chat chat-start ml-5 text-sm" key={m.id}>
+                  <Avatar userID={m.userID} />
+                  {/* <div className="chat-image avatar">
                   <div className="w-10 rounded-full">
 
                     <PiUserCircleFill size={35} className='fill-gray-500 cursor-pointer' />
                   </div>
                 </div> */}
-                <div className="chat-header dark:text-gray-200">
-                  {m.author}{' '}
-                  <time className="text-xs opacity-50">{new Intl.DateTimeFormat("en-US", {
-                    year: "numeric",
-                    month: "2-digit",
-                    day: "2-digit",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    second: "2-digit",
-                  }).format(m.timestamp)}</time>
+                  <div className="chat-header dark:text-gray-200">
+                    {m.author}{' '}
+                    <time className="text-xs opacity-50">
+                      {new Intl.DateTimeFormat('en-US', {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        second: '2-digit',
+                      }).format(m.timestamp)}
+                    </time>
+                  </div>
+                  <div>
+                  {m.hasGif ? (
+                      <img src={m.content}></img>
+                    ) : m.content.slice(0, 4) === 'http' ? (
+                      <div className="chat-bubble bg-blue-500 dark:bg-purple-600 text-white">
+                        <a
+                          href={m.content}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {m.content}
+                        </a>
+                      </div>
+                    ) : (
+                      <div className="chat-bubble bg-blue-500 dark:bg-purple-600 text-white">
+                        {m.content}
+                      </div>
+                    )}
+                  </div>
+                  <div className="chat-footer opacity-50 text-xs pt-2 dark:text-gray-100">
+                    Delivered
+                  </div>
                 </div>
-                <div>
-                  {m.hasGif ? (<img src={m.content}></img>)
-                    : (<div className="chat-bubble bg-blue-500 dark:bg-purple-600 text-white">{m.content}</div>)
-                  }</div>
-                <div className="chat-footer opacity-50 text-xs pt-2 dark:text-gray-100">
-                  Delivered
-                </div>
-              </div>
-            )}
-        </div>
-      ))}
+              )}
+            </div>
+          )
+        )}
       <div ref={msgRefContainer}></div>
     </div>
-  )
-}
+  );
+};
