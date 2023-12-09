f2c37f0795274f9dd245e9bb92784e16f1a992b8
Chuuch <daniel.chuchulev96@gmail.com>
2023-12-09 22:02:57 +0200

    Typewriter component

---
+++ src/components/Typewriter/Typewriter.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+
+interface TypewriterProps {
+
+}
+
+export const Typewriter: React.FC<TypewriterProps> = () => {
+    const [text] = useTypewriter({
+		words: [
+			'Welcome!',
+			'Connecting buddies one by one',
+			'To make communication great again!',
+		],
+		loop: true,
+		delaySpeed: 2000,
+	});
+  return (
+
+  )
+}
