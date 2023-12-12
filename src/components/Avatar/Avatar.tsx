201e71e9affda53b24d5fb4715550f0963264595
Georgi Stoychev <stoychev86@yahoo.com>
2023-12-12 22:46:20 +0000

    Feat: avatar set status manually button fix

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,53 +1,68 @@
-import { useEffect, useState } from 'react';
-import { UserDB } from '../../App';
-import { getUserByID } from '../../services/users.services';
-import Badge from '../Badge/Badge';
 import { onValue, ref } from 'firebase/database';
+import { useEffect, useState } from 'react';
+import { db } from '../../config/firebase-config';
+import { Status } from '../../utils/status';
+import Badge from '../Badge/Badge';
 
 const Avatar = ({ userID }: { userID?: string }) => {
 
-    const [userData, setUserData] = useState<UserDB | null>(null);
+    const [status, setStatus] = useState<`${Status}` | undefined>(undefined);
+    const [firstName, setFirstName] = useState<string>('');
+    const [lastName, setLastName] = useState<string>('');
+    const [avatar, setAvatar] = useState<string>('');
 
     useEffect(() => {
         if (!userID) return;
-        const fetchUser = async () => {
-            try {
-                const userByID = await getUserByID(userID);
-                setUserData(userByID);
-                console.log("userID: ", userByID);
-                console.log("data: ", userByID);
-            } catch (error) {
-                console.log("An error occurred: " + error);
+    
+        const unsubscribeStatus = onValue(ref(db, `users/${userID}/status`), (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                setStatus(data);
             }
-        }
-        fetchUser()
-        // const unsubscribe = onValue(ref(db, `users/${userID}/status`), (snapshot) => {
-        //     const data = snapshot.val();
-        //     console.log(data)
-        //     if (data) {
-        //         setUserData(data);
-        //     }
-        // });
+        });
 
-        // return () => {
-        //     unsubscribe();
-        // };
+        const unsubscribeFirstName = onValue(ref(db, `users/${userID}/firstName`), (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                setFirstName(data);
+            }
+        });
+
+        const unsubscribeLastName = onValue(ref(db, `users/${userID}/lastName`), (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                setLastName(data);
+            }
+        });
+        const unsubscribeAvatar = onValue(ref(db, `users/${userID}/avatar`), (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                setAvatar(data);
+            }
+        });
+
+        return () => {
+            unsubscribeStatus();
+            unsubscribeFirstName();
+            unsubscribeLastName();
+            unsubscribeAvatar();
+        };
     }, [userID])
 
     return (
         <>
-            {userData?.avatar?.length ? (
+            {avatar?.length ? (
                 <div className="chat-image avatar">
                     <div className="w-10 rounded-full">
-                        <img className="w-10 h-10 rounded-full" src={userData?.avatar} alt="avatar" />
-                        <Badge status={userData?.status} />
+                        <img className="w-10 h-10 rounded-full" src={avatar} alt="avatar" />
+                        <Badge status={status} />
                     </div>
                 </div >
             ) : (
                 <div className="chat-image ">
                     < div className="relative inline-flex items-center justify-center w-10 h-10  bg-gray-300 rounded-full dark:bg-gray-600 ">
-                        <span className="font-medium text-gray-500 dark:text-gray-300 ">{userData?.firstName?.charAt(0).toUpperCase() + userData?.lastName?.charAt(0).toUpperCase()}</span>
-                        <Badge status={userData?.status} />
+                        <span className="font-medium text-gray-500 dark:text-gray-300 ">{firstName?.charAt(0).toUpperCase() + lastName?.charAt(0).toUpperCase()}</span>
+                        <Badge status={status} />
                     </div >
                 </div>
             )
